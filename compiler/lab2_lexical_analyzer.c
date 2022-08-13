4e1b181c2503e90a678097291b9cdca83cfdbfe5
Gautam Chandra Saha <devgautam1231@gmail.com>
2022-08-13 10:23:27 +0530

    fixed bugs

--- compiler/lab2_lexical_analyzer.c
+++ compiler/lab2_lexical_analyzer.c
@@ -36,7 +36,7 @@ const char *tokenTypeStrings[] = {
 const char* KEYWORDS[] = {"for","int","if"};
 const char OPERATORS[] = {'<','=','+','*','-','/','%','>'};
 const char NUMBERS[] = {'0','1','2','3','4','5','6','7','8','9'};
-const char SPECIAL_CHAR[]= {'(',')',';','{','}'};;
+const char SPECIAL_CHAR[]= {'(',')',';','{','}'};
 const char COMMA=',';
 const char WHITE_SPACE=' ';
 
